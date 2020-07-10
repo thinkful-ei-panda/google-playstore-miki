97f13c24bfef85714381f7550b4057b2a9fd1250
mischkiey <mischkieycf@gmail.com>
2020-07-09 22:10:44 -0400

    Pending modifications to further specify 401 message

--- app.js
+++ app.js
@@ -1,1 +1,41 @@
-console.log('Meow');
+const express = require('express');
+const morgan = require('morgan');
+const apps = require('./playstore-apps');
+
+const app = express();
+
+app.use(morgan('dev'));
+
+app.listen(8000, () => {
+    console.log('Server meowing on PORT 8000');
+});
+
+app.get('/apps', (req, res) => {
+    let response = apps;
+
+    const sort = req.query.sort ? req.query.sort : null;
+    const genres = req.query.genres ? req.query.genres.toLowerCase() : null;
+
+    if(!sort && !genres) {
+        return res.status(200).send(response);
+    };
+
+    if(sort && ['Rating', 'App'].includes(sort)) {
+        response = response.sort((a, b) => {
+            if(a[sort] > b[sort]) {
+                return 1;
+            } else {
+                return -1
+            };
+        });
+        return res.status(200).send(response);
+    }
+
+    if(genres && ['action', 'puzzle', 'strategy', 'casual', 'arcade', 'card'].includes(genres)) {
+        response = response.filter(app => app.Genres.toLowerCase().includes(genres));
+        return res.status(200).send(response);
+    };
+
+    res.status(401).send('Bad request');
+
+});
